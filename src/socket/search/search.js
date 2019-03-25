d33e041566b6dfd906b25a1b4c8f6ce20e26bade
Hamit Zor <thenrerise@gmail.com>
2019-03-26 02:31:05 +0300

    error handlers added for send methods

--- src/socket/search/search.js
+++ src/socket/search/search.js
@@ -46,7 +46,7 @@ class SearchSocket {
           ws.send(JSON.stringify({
             status: false,
             message: 'videoId is not valid'
-          }))
+          }), (err) => { this.logger(err) })
         }
         else {
           this.saveBase64Image(data.base64Image, (filePath, err) => {
@@ -60,14 +60,15 @@ class SearchSocket {
                   ws.send(JSON.stringify({
                     status: false,
                     message: 'Internal Server Error'
-                  }))
+                  }), (err) => { this.logger(err) })
+
                   this.logger.error(err)
                 }
                 else {
                   ws.send(JSON.stringify({
                     status: true,
                     result: JSON.parse(stdout)
-                  }))
+                  }), (err) => { this.logger(err) })
                 }
               })
             }
