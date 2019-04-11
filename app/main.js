584196e47dd345ae2c4d61d89daf7c1edaae07a1
Kagan <kgnugur@gmail.com>
2019-04-11 22:30:13 +0300

    Video route

--- app/main.js
+++ app/main.js
@@ -1,40 +1,46 @@
-import "@babel/polyfill"
-import "source-map-support/register"
-import express from "express"
-import cookieParser from "cookie-parser"
-import bodyParser from "body-parser"
-import compression from "compression"
-import path from "path"
-import SearchWebSocketServerInitializer from "./web-socket/search-web-socket-server-initializer"
-import config from "../app.config"
-import http from "http"
+import '@babel/polyfill';
+import 'source-map-support/register';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import path from 'path';
+import SearchWebSocketServerInitializer from './web-socket/search-web-socket-server-initializer';
+import config from '../app.config';
+import http from 'http';
+import videoRoutes from './routes/video';
 
-const app = express()
-const server = http.createServer(app)
-const port = config.server.port
-const domain = config.server.domain
+const app = express();
+const server = http.createServer(app);
+const port = config.server.port;
+const domain = config.server.domain;
 
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname)));
 
-app.use(compression())
-app.use(cookieParser())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(path.resolve(__dirname)))
+new SearchWebSocketServerInitializer(server).attachHandlers();
 
-new SearchWebSocketServerInitializer(server).attachHandlers()
+app.get('/', function(req, res) {
+  res.sendFile(path.resolve(__dirname, '../client-test-pages/home.html'));
+});
 
-app.get("/", function (req, res) {
-  res.sendFile(path.resolve(__dirname, "../client-test-pages/home.html"))
-})
+app.get('/test', function(req, res) {
+  res.sendFile(
+    path.resolve(__dirname, '../client-test-pages/test-page-home.html')
+  );
+});
 
-app.get("/test", function (req, res) {
-  res.sendFile(path.resolve(__dirname, "../client-test-pages/test-page-home.html"))
-})
+app.get('/test/search-test', function(req, res) {
+  res.sendFile(
+    path.resolve(__dirname, '../client-test-pages/test-page-search.html')
+  );
+});
 
-app.get("/test/search-test", function (req, res) {
-  res.sendFile(path.resolve(__dirname, "../client-test-pages/test-page-search.html"))
-})
+app.use(videoRoutes);
 
 server.listen(port, () => {
-  console.log(`Application is online at ${domain}:${port}`)
-})
+  console.log(`Application is online at ${domain}:${port}`);
+});
