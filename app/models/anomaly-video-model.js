bc825979d40489a2b7927bf0a17bb737cc3eaec1
Zeromika <umutguler97@gmail.com>
2019-05-04 18:48:04 +0300

    SQL fix

--- app/models/anomaly-video-model.js
+++ app/models/anomaly-video-model.js
@@ -6,10 +6,9 @@ import db from "../util/db-connection-pool"
 
 export default class AnomalyDetectedModel {
   static fetchById(videoId) {
-    return db.execute("SELECT anomaly_id AS ID, a.video_id, c.rule_id, b.frame_no AS frameNo, b.left_x AS left, b.top_y AS top, width, height, related_function_name FROM video_detected_anomaly a JOIN detected_anomalies b ON b.detected_anomaly_id = a.detected_anomaly_id JOIN anomalies c ON b.rule_id=c.anomaly_id WHERE a.video_id = ? and rule_id != 2", [videoId])
-  }
+    return db.execute("SELECT anomaly_id AS ID, a.video_id, b.rule_id, b.frame_no AS 'frameNo', b.left_x 'left', b.top_y AS 'top', width, height, related_function_name FROM video_detected_anomaly a JOIN detected_anomalies b ON b.detected_anomaly_id = a.detected_anomaly_id JOIN anomalies c ON b.rule_id=c.anomaly_id WHERE a.video_id = 1 AND rule_id != 2", [videoId])}
 
   static fetchLineCrossingById(videoID){
-    return db.execute("SELECT anomaly_id AS ID, a.video_id, c.rule_id, b.frame_no AS frameNo, b.left_x AS left, b.top_y AS top, width, height, related_function_name FROM video_detected_anomaly a JOIN detected_anomalies b ON b.detected_anomaly_id = a.detected_anomaly_id JOIN anomalies c ON b.rule_id=c.anomaly_id WHERE a.video_id = ? and rule_id = 2", [videoId])
+    return db.execute("SELECT anomaly_id AS ID, a.video_id, b.rule_id, b.frame_no AS 'frameNo', b.left_x 'left', b.top_y AS 'top', width, height, related_function_name FROM video_detected_anomaly a JOIN detected_anomalies b ON b.detected_anomaly_id = a.detected_anomaly_id JOIN anomalies c ON b.rule_id=c.anomaly_id WHERE a.video_id = ? AND rule_id = 2", [videoId])
   }
 }
